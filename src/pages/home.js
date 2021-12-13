306669d0d20a78decaf0b170e00494ddf5d6f86c
pythagoras-dev <pythagoras.dev15@gmail.com>
2021-12-13 21:28:15 +0100

    commit 15

--- src/pages/home.js
+++ src/pages/home.js
@@ -18,17 +18,54 @@ const Home = ({ toggle, isOpen }) => {
       <section className="bg-black ">
         <div className="inner mx-auto max-w-6xl px-4 sm:px-6">
           <img src={homeHero} alt="homeImage" className="homeImage" />
-          <div class="grid grid-rows-2 md:grid-flow-col gap-4 homeSection2 ">
-            <div class="row-span-2 col-span-2 ...">
+          {/* <div className="flex max-w-6xl mx-auto py-11 section3">
+            <div className="text-white">
+              <h1 className="heading">
+                WELCOME TO THE <br />
+                BORED APE YACHT CLUB
+              </h1>
+              <p>
+                Loripsuem m dolor sit amet, consectetur adipiscing elit. Proin
+                bibendum imperdiet blandit.consectetur adipiscing elit. Proin
+                bibendum imperdiet blandit.consectetur adipiscing elit. Proin
+                bibendum imperdiet blandit.
+              </p>
+            </div>
+            <div className="md:grid-flow-col">
+              <div class="grid grid-cols-2 gap-7 picture-grid">
+                <div>
+                  <img src={ape1} alt="homeImage" className="gridImage" />
+                </div>
+                <div>
+                  <img src={ape2} alt="homeImage" className="gridImage" />
+                </div>
+                <div>
+                  <img src={ape3} alt="homeImage" className="gridImage" />
+                </div>
+
+                <div>
+                  <img src={ape4} alt="homeImage" className="gridImage" />
+                </div>
+              </div>
+            </div>
+          </div> */}
+
+          <div class="grid grid-rows-3 grid-rows-3 grid-flow-col gap-4 homeSection2">
+            <div class="row-span-1 col-span-2">
               <div className="text-white">
-                <h1 className="heading">WELCOME TO THE BORED APE YACHT CLUB</h1>
+                <h1 className="heading">
+                  WELCOME TO THE <br />
+                  BORED APE YACHT CLUB
+                </h1>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                  Loripsuem m dolor sit amet, consectetur adipiscing elit. Proin
+                  bibendum imperdiet blandit.consectetur adipiscing elit. Proin
+                  bibendum imperdiet blandit.consectetur adipiscing elit. Proin
                   bibendum imperdiet blandit.
                 </p>
               </div>
             </div>
-            <div class="row-span-3 ... text-white">
+            <div class="row-span-3 text-white picture-grid">
               <div class="grid grid-cols-2 gap-2">
                 <div>
                   <img src={ape1} alt="homeImage" />
@@ -45,7 +82,49 @@ const Home = ({ toggle, isOpen }) => {
                 </div>
               </div>
             </div>
-            <div class="col-span-2 ...">02</div>
+          </div>
+
+          {/* */}
+
+          <div class="grid grid-rows-1 md:grid-flow-col gap-4 homeSection2">
+            <div class="row-span-2 col-span-2 ...">
+              <div className="text-white">
+                <h4 className="mb-5 mr-9">
+                  FAIR DISTRIBUTION <br /> (BONDING CURVES ARE A PONZI)
+                </h4>
+                <p>
+                  Loripsuem m dolor sit amet, consectetur adipiscing elit. Proin
+                  bibendum imperdiet blandit
+                </p>
+              </div>
+            </div>
+            <div class="row-auto text-white">
+              <p>
+                Loripsuem m dolor sit amet, consectetur adipiscing elit. Proin
+                bibendum imperdiet blandit.
+              </p>
+            </div>
+          </div>
+
+          {/* */}
+          <div className="cta text-black flex justify-evenly">
+            <div>
+              <h1 className="heading">BUY AN APE</h1>
+            </div>
+            <div>
+              <p>
+                {" "}
+                The initial sale has sold out. To get your
+                <br /> Bored Ape, check out the collection on <br />
+                OpenSea.
+              </p>
+            </div>
+            <div>
+              <button className="cta-btn bg-black text-yellow-400 py-4 px-4 rounded-md">
+                BUY AN APE ON <br />
+                OPENSEA
+              </button>
+            </div>
           </div>
         </div>
       </section>
